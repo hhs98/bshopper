6812b7151c1722ca0a71a9ba0ae9cb0dd5cbbb93
Hasibul Hasan Siju <siju35-168@diu.edu.bd>
2021-03-31 15:19:35 +0600

    Bismill-Allah

--- bshopper/settings.py
+++ bshopper/settings.py
@@ -68,8 +68,8 @@ SIMPLE_JWT = {
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
