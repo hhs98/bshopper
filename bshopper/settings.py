3d6674e3c0ca6b74f26b3d182022c2badb621b3d
Hasibul Hasan Siju <siju35-168@diu.edu.bd>
2021-03-31 19:32:42 +0600

    StaticFiles

--- bshopper/settings.py
+++ bshopper/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/images/'
+MEDIA_URL = 'static/images/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
