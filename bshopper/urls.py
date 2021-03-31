3dfa3e2b62ca031b3e0e46b05b93e4be8dff0d89
Hasibul Hasan Siju <siju35-168@diu.edu.bd>
2021-03-31 18:58:52 +0600

    StaticFiles

--- bshopper/urls.py
+++ bshopper/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
 
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
